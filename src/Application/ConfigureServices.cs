375ddc6e87dfc0c81e657417f840b3ec04b7d5d9
Danil <47692381+SelDanilEv@users.noreply.github.com>
2024-06-16 00:12:45 +0400

    Initial commit

---
+++ src/Application/ConfigureServices.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using FluentValidation;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Defender.ServiceTemplate.Application.Common.Interfaces;
+using Defender.ServiceTemplate.Application.Services;
+
+namespace Defender.ServiceTemplate.Application;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+        services.RegisterServices();
+
+        return services;
+    }
+
+    private static IServiceCollection RegisterServices(this IServiceCollection services)
+    {
+        services.AddTransient<IService, Service>();
+
+        return services;
+    }
+}
